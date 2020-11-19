29b5dfe93f7358bc6d32635599ca2847b48341e3
Sanel Gluhic <sagl19ab@student.cbs.dk>
2020-11-19 11:57:07 +0100

    må ting rettet

--- API-filer/routes/clients.js
+++ API-filer/routes/clients.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const Client = require('../models/clients');
 const mongoose = require('mongoose');
+
 mongoose.set('useFindAndModify', false); // Ellers kommer der fejl op
 
 // [1] Hent alle eksisterende kunder
